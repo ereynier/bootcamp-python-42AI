9e2c96ab9e70527237201bc568c948682a5331a2
ereynier <ereynier@student.le-101.fr>
2020-05-26 20:05:17 +0200

    norme

--- d01/ex01/game.py
+++ d01/ex01/game.py
@@ -2,7 +2,7 @@ class GotCharacter():
     def __init__(self, first_name, is_alive=True):
         self.first_name = first_name
         self.is_alive = is_alive
-    
+
 
 class Stark(GotCharacter):
     def __init__(self, first_name=None, is_alive=True):
